de7cbeba3b81b2832609d7b2401fe0e513011bdd
Shomari Roberts <shomariroberts@gmail.com>
2021-01-27 22:16:22 -0500

    build out header and footer to define page layout

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,34 +1,38 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from '../../common';
+import cityspireLogo from '../../../assets/imgs/cityspireLogo.png';
+
+import { Layout, Avatar, Button, Image, Space } from 'antd';
+
+import { UserOutlined } from '@ant-design/icons';
+
+const { Header, Footer } = Layout;
 
 function RenderHomePage(props) {
   const { userInfo, authService } = props;
   return (
-    <div>
-      <h1>Hi {userInfo.name} Welcome to Labs Basic SPA</h1>
-      <div>
-        <p>
-          This is an example of a common example of how we'd like for you to
-          approach components.
-        </p>
-        <p>
-          <Link to="/profile-list">Profiles Example</Link>
-        </p>
-        <p>
-          <Link to="/example-list">Example List of Items</Link>
-        </p>
-        <p>
-          <Link to="/datavis">Data Visualizations Example</Link>
-        </p>
-        <p>
-          <Button
-            handleClick={() => authService.logout()}
-            buttonText="Logout"
-          />
-        </p>
-      </div>
-    </div>
+    <Layout className="layout">
+      <Header
+        style={{
+          backgroundColor: 'white',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Image preview={false} src={cityspireLogo} style={{ width: '120px' }} />
+        <Space size="large">
+          <Space size="small">
+            <Avatar size="small" icon={<UserOutlined />} />
+            {userInfo.name}
+            {console.log(userInfo)}
+          </Space>
+          <Button onClick={() => authService.logout()}>Logout</Button>
+        </Space>
+      </Header>
+      <Footer style={{ textAlign: 'center' }}>
+        Cityspire ©2021 Created by Labspt15-cityspire-g
+      </Footer>
+    </Layout>
   );
 }
 export default RenderHomePage;
