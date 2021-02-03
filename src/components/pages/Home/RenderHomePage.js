bca9d1173bd3cbdc20f7fa110823effc15092ef2
Britney Parkerson <justbritneyp1989@gmail.com>
2021-02-03 01:02:50 -0500

    Added some styles to the homepage. Search form added but not functional, images added, mock paragrah added.

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import cityspireLogo from '../../../assets/imgs/cityspireLogo.png';
+import cities from '../../../assets/imgs/cities.png';
+import citylife from '../../../assets/imgs/citylife.jpg';
+import map from '../../../assets/imgs/map2.jpg';
 
-import { Layout, Avatar, Button, Image, Space } from 'antd';
+import { Layout, Avatar, Button, Image, Space, Input, Form } from 'antd';
 
 import { UserOutlined } from '@ant-design/icons';
 
@@ -20,6 +23,7 @@ function RenderHomePage(props) {
         }}
       >
         <Image preview={false} src={cityspireLogo} style={{ width: '120px' }} />
+
         <Space size="large">
           <Space size="small">
             <Avatar size="small" icon={<UserOutlined />} />
@@ -29,6 +33,76 @@ function RenderHomePage(props) {
           <Button onClick={() => authService.logout()}>Logout</Button>
         </Space>
       </Header>
+      <Image
+        preview={false}
+        src={cities}
+        style={{ width: '100%', height: '100px' }}
+      />
+      <Space
+        size="large"
+        align="center"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          margin: '2%',
+          textAlign: 'center',
+        }}
+      >
+        <Form class="search" action="">
+          <Input
+            type="text"
+            placeholder="Search.."
+            name="search"
+            style={{
+              borderRadius: '1%',
+              textAlign: 'center',
+              width: '60%',
+            }}
+          />
+          <Button
+            type="submit"
+            style={{
+              width: '30%',
+              borderRadius: '15%',
+              textAlign: 'center',
+              margin: '2%',
+            }}
+          >
+            {' '}
+            Search
+          </Button>
+        </Form>{' '}
+      </Space>
+
+      <Space size="large" align="center">
+        <p
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            margin: '2%',
+            textAlign: 'center',
+            fontSize: '1.5rem',
+          }}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <Image
+          preview={false}
+          src={map}
+          style={{
+            width: '100%',
+            height: '100x',
+            marginBottom: '5%',
+          }}
+        />{' '}
+      </Space>
+
+      <Image
+        preview={false}
+        src={citylife}
+        style={{ width: '100%', height: '100px' }}
+      />
       <Footer style={{ textAlign: 'center' }}>
         Cityspire ©2021 Created by Labspt15-cityspire-g
       </Footer>
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import {
 
 import { Security, LoginCallback, SecureRoute } from '@okta/okta-react';
 import 'antd/dist/antd.less';
+import 'antd/dist/antd.css';
 
 import { NotFoundPage } from './components/pages/NotFound';
 import { ExampleListPage } from './components/pages/ExampleList';
@@ -40,23 +41,22 @@ function App() {
   };
 
   return (
-    
     <Security {...config} onAuthRequired={authHandler}>
-    <Switch>
-      <Route path="/login" component={LoginPage} />
-      <Route path="/implicit/callback" component={LoginCallback} />
-      {/* any of the routes you need secured should be registered as SecureRoutes */}
-      <SecureRoute
-        path="/"
-        exact
-        component={() => <HomePage LoadingComponent={LoadingComponent} />}
-      />
-      <SecureRoute path="/example-list" component={ExampleListPage} />
-      
-      <SecureRoute path="/profile-list" component={ProfileListPage} />
-      <SecureRoute path="/datavis" component={ExampleDataViz} />
-      <Route component={NotFoundPage} />
-    </Switch>
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/implicit/callback" component={LoginCallback} />
+        {/* any of the routes you need secured should be registered as SecureRoutes */}
+        <SecureRoute
+          path="/"
+          exact
+          component={() => <HomePage LoadingComponent={LoadingComponent} />}
+        />
+        <SecureRoute path="/example-list" component={ExampleListPage} />
+
+        <SecureRoute path="/profile-list" component={ProfileListPage} />
+        <SecureRoute path="/datavis" component={ExampleDataViz} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </Security>
   );
 }
