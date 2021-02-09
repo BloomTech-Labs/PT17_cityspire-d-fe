31341cba6e30572d9dd4f8764711e272a198ddb6
Shomari Roberts <shomariroberts@gmail.com>
2021-02-09 16:41:41 -0500

    completed fetchCityData action

--- src/state/actions/cityData.js
+++ src/state/actions/cityData.js
@@ -1,7 +1,27 @@
+import axios from 'axios';
+
 export const FETCHING_CITY_START = 'FETCHING_CITY_START';
 export const FETCHING_CITY_SUCCESS = 'FETCHING_CITY_SUCCESS';
 export const FETCHING_CITY_ERROR = 'FETCHING_CITY_ERROR';
 
-export const fetchCityData = city => dispatch => {
-  dispatch({ type: FETCHING_CITY_START });
+export const fetchCityData = cityInfo => {
+  return async dispatch => {
+    dispatch({ type: FETCHING_CITY_START });
+
+    console.log(cityInfo);
+
+    try {
+      const res = await axios.post(
+        `http://cityscape-203.eba-ijacxhm2.us-east-1.elasticbeanstalk.com/api/get_data`,
+        cityInfo
+      );
+
+      dispatch({
+        type: FETCHING_CITY_SUCCESS,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({ type: FETCHING_CITY_ERROR, payload: err });
+    }
+  };
 };
