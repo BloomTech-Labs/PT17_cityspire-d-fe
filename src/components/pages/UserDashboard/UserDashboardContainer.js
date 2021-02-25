2ba8d4dd57e36a8d353652135bb6355d74e6fa2c
Shomari Roberts <shomariroberts@gmail.com>
2021-02-24 22:30:28 -0500

    added loader for when fetching savedCities

--- src/components/pages/UserDashboard/UserDashboardContainer.js
+++ src/components/pages/UserDashboard/UserDashboardContainer.js
@@ -2,13 +2,23 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchSavedCity, unsaveCity } from '../../../state/actions';
 
-import { Header, Footer } from '../../common';
+import { Spin } from 'antd';
+import { Header, Footer } from '../../common/';
 import RenderUserDashboard from './RenderUserDahsboard';
+
+const spinStyle = {
+  textAlign: 'center',
+  position: 'absolute',
+  top: '46%',
+  width: '100%',
+  margin: 'auto',
+};
 
 const UserDashboardContainer = ({
   fetchSavedCity,
   savedCities,
   unsaveCity,
+  isFetching,
 }) => {
   useEffect(() => {
     fetchSavedCity(localStorage.getItem('token'));
@@ -17,7 +27,16 @@ const UserDashboardContainer = ({
   return (
     <>
       <Header />
-      <RenderUserDashboard savedCities={savedCities} unsaveCity={unsaveCity} />
+      {isFetching ? (
+        <div style={spinStyle}>
+          <Spin tip="Loading..." size="large"></Spin>
+        </div>
+      ) : (
+        <RenderUserDashboard
+          savedCities={savedCities}
+          unsaveCity={unsaveCity}
+        />
+      )}
       <Footer />
     </>
   );
