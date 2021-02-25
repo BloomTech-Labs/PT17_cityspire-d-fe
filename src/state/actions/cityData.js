c44944f180ca5894e703b871179d6a2627376081
Shomari Roberts <shomariroberts@gmail.com>
2021-02-25 08:41:13 -0500

    updated DS API link

--- src/state/actions/cityData.js
+++ src/state/actions/cityData.js
@@ -10,7 +10,7 @@ export const fetchCityData = cityInfo => {
 
     try {
       const res = await axios.post(
-        `http://cityscape-203.eba-ijacxhm2.us-east-1.elasticbeanstalk.com/api/get_data`,
+        `https://cityscape-203.eba-ijacxhm2.us-east-1.elasticbeanstalk.com/api/get_data`,
         cityInfo
       );
 
