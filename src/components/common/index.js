d03b1ac6382837923ce16980c4863b5ee63794f5
Shomari Roberts <shomariroberts@gmail.com>
2021-02-24 21:47:38 -0500

    centralized component imports

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -3,6 +3,20 @@ import FormInput from './FormInput';
 import List from './List';
 import LoadingComponent from './LoadingComponent';
 import Button from './Button';
+import Header from './Header';
+import Footer from './Footer';
+import SearchForm from './SearchForm';
+import MapboxGLMap from './MapboxGLMap';
 // notice we're building out a 'package' of reusables here and exporting them as an object of component properties.
 // to use this, simply `import {foo, bar, baz} from '<path-to-this-directory>/ReusableComponents';`
-export { FormButton, FormInput, List, LoadingComponent, Button };
+export {
+  FormButton,
+  FormInput,
+  List,
+  LoadingComponent,
+  Button,
+  Header,
+  Footer,
+  SearchForm,
+  MapboxGLMap,
+};
