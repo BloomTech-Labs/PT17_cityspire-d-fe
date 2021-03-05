6e83532168c3d23bd5522e72dbcec0d7029e8ba9
Shomari Roberts <shomariroberts@gmail.com>
2021-03-05 09:22:53 -0500

    removed viewCity from state

--- src/state/reducers/cityOperations.js
+++ src/state/reducers/cityOperations.js
@@ -9,7 +9,6 @@ import {
 const initialState = {
   isFetching: false,
   savedCities: [],
-  viewCity: {},
   isSaved: false,
   error: null,
 };
@@ -18,7 +17,7 @@ export const cityOperationsReducer = (state = initialState, action) => {
     case PIN_CITY:
       return {
         ...state,
-        savedCities: [...state.savedCities, action.payload],
+        savedCities: [...action.payload],
         isSaved: true,
       };
     case UNPIN_CITY:
