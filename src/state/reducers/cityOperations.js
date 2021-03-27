c00ca98edb4ce95808c8f100f2afe0569c91cdee
Kim Ly Buck <kim.3993@gmail.com>
2021-03-26 22:52:42 -0500

    fixed reducer

--- src/state/reducers/cityOperations.js
+++ src/state/reducers/cityOperations.js
@@ -17,7 +17,7 @@ export const cityOperationsReducer = (state = initialState, action) => {
     case PIN_CITY:
       return {
         ...state,
-        savedCities: [...action.payload],
+        savedCities: [...state.savedCities, action.payload],
         isSaved: true,
       };
     case UNPIN_CITY:
