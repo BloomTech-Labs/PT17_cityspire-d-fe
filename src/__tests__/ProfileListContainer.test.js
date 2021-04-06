aa3df896f3f25eb5cd37cbc3a5f63aba0ce876d2
Catherine <catherinesjkim@gmail.com>
2021-04-06 06:46:04 -0700

    Add Updated Files ### Locations: - package.json - SearchForm.js - RenderPinnedCity.js - ProfileListContainer.js - RenderUserDashboard.js - CityOperations.js ### Details: 1) Updated all lines with  to  on RenderPinnedCity.js and RenderUserDashboard.js because we are dealing with cities and not items 2) Took out the spread operator state from line 20 in the cityoperations.js file 3) Took out the BroowserRouter import from the App.js file ## Commited Features 1 | Updated text font type and color to make the landing page to look more modern and similar to PM's spec.

--- src/__tests__/ProfileListContainer.test.js
+++ src/__tests__/ProfileListContainer.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {
   render,
-  screen,
-  waitForElementToBeRemoved,
+  // screen,
+  // waitForElementToBeRemoved,
   act,
-  getByTestId,
+  // getByTestId,
 } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ProfileListPage } from '../components/pages/ProfileList';
